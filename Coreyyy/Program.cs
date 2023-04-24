8f106595326fa9ce58e42f9af1456a9f7e98d1eb
Пакалов Павел Павлович <pakalov.pp1640@okeit.edu>
2023-04-24 10:35:22 +0300

    Добавьте файлы проекта.

---
+++ Coreyyy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CalculationLib;
+namespace Coreyyy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сложение:");
+            Console.WriteLine(CalculationLib.Castle1.Plus());
+            Console.WriteLine("");
+            Console.WriteLine("Вычитание:");
+            Console.WriteLine(CalculationLib.Castle1.Minus());
+            Console.WriteLine("");
+            Console.WriteLine("Умножение:");
+            Console.WriteLine(CalculationLib.Castle1.Multiplication());
+            Console.WriteLine("");
+            Console.WriteLine("Деление:");
+            Console.WriteLine(CalculationLib.Castle1.Division());
+        }
+    }
+}
---
+++ USQLCSharpProject123/Class1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CalculationLib
+{
+    public static class Castle1
+    {
+       public static double Plus()
+        {
+           double a = Convert.ToDouble(Console.ReadLine());
+           double b = Convert.ToDouble(Console.ReadLine());
+            return a + b;
+        }
+        public static double Minus()
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            return a - b;
+        }
+        public static double Multiplication()
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            return a * b;
+        }
+        public static double Division()
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            return a / b;
+        }
+    }
+}
